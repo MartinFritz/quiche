e73b3283b18471f2452e4e0e4f4d30c0873dc965
MartinFritz <45102283+MartinFritz@users.noreply.github.com>
2023-09-18 14:21:13 +0200

    added nix patch introduced in main repo by the user "ghedo"

--- apps/src/sendto.rs
+++ apps/src/sendto.rs
@@ -35,7 +35,9 @@ pub fn detect_gso(socket: &mio::net::UdpSocket, segment_size: usize) -> bool {
     use nix::sys::socket::sockopt::UdpGsoSegment;
     use std::os::unix::io::AsRawFd;
 
-    setsockopt(socket.as_raw_fd(), UdpGsoSegment, &(segment_size as i32)).is_ok()
+    let fd = unsafe { std::os::fd::BorrowedFd::borrow_raw(sock.as_raw_fd()) };
+
+    setsockopt(&fd, TxTime, &config)?;
 }
 
 /// For non-Linux, there is no GSO support.
